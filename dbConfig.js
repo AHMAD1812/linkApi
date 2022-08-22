530dc30596ad00f3ae7c8a88cd54ea039e234698
AHMAD1812 <ahmadimran1812@gmail.com>
2022-08-22 23:56:49 +0500

    first commit

---
+++ dbConfig.js
@@ -0,0 +1,13 @@
+const config = {
+    user: 'link',
+    password: 'tlc1985&Going',
+    server: 'link-reporting.database.windows.net',
+    database: 'reporting',
+    options: {
+        trustedconnection: true,
+        enableArithAbort: true,
+        instancename:'SQLEXPRESS'
+    }
+}
+
+module.exports = config;
---
+++ dbOperation.js
@@ -0,0 +1,29 @@
+var config = require('./dbConfig');
+const sql = require('mssql');
+
+async function getUsers() {
+    try {
+        let conn = await sql.connect(config);
+        let users = await conn.request().query("SELECT * FROM SALESREP_ZIPCODE");
+        return users.recordsets;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getUser(zip) {
+    try {
+        let conn = await sql.connect(config);
+        let user = await conn.request()
+            .input('input_params',sql.Int,zip)
+            .query("SELECT * FROM SALESREP_ZIPCODE where ZIPCODE = @input_params");
+        return user.recordsets;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getUsers: getUsers,
+    getUser:getUser
+}
