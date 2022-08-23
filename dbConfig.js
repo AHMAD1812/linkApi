eb335a6d8866d24996cf19d89a6e05d236ac1bac
AHMAD1812 <ahmadimran1812@gmail.com>
2022-08-23 11:24:06 +0500

    changes

--- dbConfig.js
+++ dbConfig.js
@@ -3,13 +3,16 @@ const config = {
     password: 'tlc1985&Going',
     server: 'link-reporting.database.windows.net',
     database: 'reporting',
-    requestTimeout: 180000, // for timeout setting
-    connectionTimeout: 180000, // for
     options: {
         trustedconnection: true,
         enableArithAbort: true,
         encrypt:true,
         instancename:'SQLEXPRESS'
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 60000
     }
 }
 
