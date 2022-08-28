c33ef4248ca953ec1e31031849af26fbd446992f
AHMAD1812 <ahmadimran1812@gmail.com>
2022-08-28 11:33:47 +0500

    changes

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if(isset($_GET['zip'])){
 
    echo json_encode($members);
 }else if(isset($_GET['zipcode']) && isset($_GET['brand'])){
-   $sql = "SELECT * FROM SALESREP_ZIP_VENDOR";
+   $sql = "SELECT * FROM SALESREP_ZIPCODE";
    $query = $conn->prepare($sql);
    $query->execute();
    $members = $query->fetchAll(PDO::FETCH_ASSOC);
