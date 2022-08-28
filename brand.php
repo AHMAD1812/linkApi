7c64ec5f2db4c24a9a2653cf8227f888f05e3666
AHMAD1812 <ahmadimran1812@gmail.com>
2022-08-28 11:14:05 +0500

    changes

--- brand.php
+++ brand.php
@@ -1,22 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-
-// SQL server configuration
-$serverName = "link-reporting.database.windows.net";
-$dbUsername = "link";
-$dbPassword = "tlc1985&Going";
-$dbName = "reporting";
-
-// Create database connection
-try {
-    $conn = new PDO("sqlsrv:Server=$serverName;Database=$dbName", $dbUsername, $dbPassword);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error connecting to SQL Server: " . $e->getMessage());
-}
+require('./config');
 
 $sql = "SELECT * FROM SALESREP_ZIP_VENDOR";
 $query = $conn->prepare($sql);
